6c41447fe6d944074182dd784bb4813508b929b6
sekarayusavitri <savitrisekar10@gmail.com>
2022-02-24 21:51:23 +0700

    [sekar] - create new branch - create new class HomeActivity.kt

---
+++ app/src/main/java/com/allana/food_recipe_app/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.allana.food_recipe_app.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.allana.food_recipe_app.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
